c9453f43825946349cf1790357cf5d25e9f3aea7
Umar <11750115076@students.uin-suska.ac.id>
2022-06-22 23:58:00 +0700

    donwload pdf

--- resources/views/admin/konsultasi/index.blade.php
+++ resources/views/admin/konsultasi/index.blade.php
@@ -6,6 +6,7 @@ $title = "Konsultasi Zakat"
 @push('styles')
     <!-- Data Table Css -->
     <link rel="stylesheet" type="text/css" href="{{ asset('adminty\bower_components\datatables.net-bs4\css\dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('adminty\bower_components\datatables.net-buttons\css\buttons.dataTables.min.css') }}">
 @endpush
 
 @section('content')
@@ -103,9 +104,18 @@ $title = "Konsultasi Zakat"
     <!-- data-table js -->
     <script src="{{ asset('adminty\bower_components\datatables.net\js\jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('adminty\bower_components\datatables.net-bs4\js\dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('adminty\bower_components\datatables.net-buttons\js\dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('adminty\bower_components\datatables.net-buttons\js\buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('adminty\bower_components\datatables.net-buttons\js\pdfmake.min.js') }}"></script>
+    <script src="{{ asset('adminty\bower_components\datatables.net-buttons\js\vfs_fonts.js') }}"></script>
     <script>
         $(document).ready(function() {
-            $('#simpletable').DataTable();
+            $('#simpletable').DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    'pdf'
+                ]
+            });
         });
     </script>
 @endpush
